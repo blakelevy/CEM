438f8e23c4f80377d86cac463813e82b6d29573c
Ashar <lawa0014@umn.edu>
2013-03-18 20:20:11 -0500

    added constants file

---
+++ constants.m
@@ -0,0 +1,10 @@
+% example script
+%% Project #1
+% Authors: Adedayo Lawal and Blake Levy
+clc;clear;
+%% Set up Parameters
+c = 299792458;
+mu = (4*pi)*1e-7;
+epsilon = 1/(mu*c^2);
+e_top = epsilon;
+e_bottom = 3*epsilon;
